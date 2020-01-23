ab416c3da6f3d0e88367f2b83828673e11e0931a
lintechnokrats-dev <abhay_a@lintechnokrats.in>
2020-01-23 16:23:26 +0530

    Janapriya welcome page

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" class="is-animating">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Janapriya</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet">
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <link rel="stylesheet" href="./assets/main.css?id=00696">
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo">
+            <a class="nav-brand"><img src="jp_logo.png" alt="site-logo" style="width: 350px;"/></a>
+        </div>
+    </header>
+    <main class="wrapper transition-fade" id="swup">
+        <div class="wrapper-inner">
+            <h1 g-component="SeparateText" class="fill-title"> New Janapriya Super Speciality Hospital </h1>
+           
+
+                <div class="form-group">
+                    <div class="text-center mt-8">
+                        <span class="mr-2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PRI/DID No:</span>
+                        <span class="mr-2">8061355555</span>
+					</div>
+					<div class="text-center mt-5">
+                        <span class="mr-2">Mobile Number:</span>
+                        <span class="mr-2">6364605555</span>
+					</div>
+					<div class="text-center mt-5">
+                        <span class="mr-2">&nbsp;Toll Free Number:</span>
+                        <span class="mr-2">18005723555 &nbsp;</span>
+					</div>
+                   
+                </div>
+                
+                
+                <br>
+                 <h1 g-component="SeparateText" class="fill-title" style="font-size:22px;">JP2 Address: </h1>
+                 
+                 <div class="form-group">
+                    <div class="text-center mt-5">
+                        <span class="mr-2">No: 2M,-319,East of NGEF Layout,<br> Opp S.B.I Bank,Kasturinagar, <br> Bangalore 560043.</span>
+                    </div>
+				</div>
+				
+				
+				 <br>
+                 <h1 g-component="SeparateText" class="fill-title" style="font-size:22px;">Corporate office Address:</h1>
+                 
+                 <div class="form-group">
+                    <div class="text-center mt-5">
+                        <span class="mr-2">Complex 2, C/330,1st floor, <br>MM Party Hall OMBR layout,Banaswadi, <br>Bangalore 560043. <br>Land mark Opp to CMR Law College.</span>
+                    </div>
+				</div>
+                
+                
+
+
+        </div>
+    </main>
+    <script src="./assets/app.js"></script>
+</body>
+
+</html>
